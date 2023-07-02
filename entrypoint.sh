c1b6581505c74387071de2a4415b93b431fad620
Matthew Morley <matthew.morley.ca@gmail.com>
2023-07-02 09:36:08 -0700

    Add debug prints

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+pwd
+ls
+
 git config --global --add safe.directory $3
 stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data /tmp/stm-workspace -import $1
 headless-build.sh -data /tmp/stm-workspace -build $2
